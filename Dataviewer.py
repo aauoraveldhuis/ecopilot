2a17adb41e531e2d27c6812d0a7c89f1aa107632
Hamza <Hamzi.habanakeh000@gmail.com>
2023-05-02 10:46:39 +0200

    grafer

--- Dataviewer.py
+++ Dataviewer.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os 
-
+import matplotlib.pyplot as plt
 
 directory = r"C:\Users\Hamza\ecopilot"
 os.chdir(directory)
@@ -8,7 +8,16 @@ os.chdir(directory)
 df= pd.read_csv(r"C:\Users\Hamza\ecopilot\df.csv")
 df.dropna()
 
-
 print(df.min())
 print(df.max())
+
+fig, axes = plt.subplots(nrows=3, ncols=1)
+
+df['avstånd till framförvarande fordon (sek)'].plot(ax=axes[0])
+df['hastighet'].plot(ax=axes[1])
+df['acc'].plot(ax=axes[2])
+
+plt.show()
+
+
 
