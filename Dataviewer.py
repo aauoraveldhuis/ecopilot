32405e83f94596327a383daffb611f99439e75e9
Hamza <Hamzi.habanakeh000@gmail.com>
2023-05-04 11:43:41 +0200

    finare grafer

--- Dataviewer.py
+++ Dataviewer.py
@@ -13,6 +13,17 @@ print(df.min())
 fig, axes = plt.subplots(nrows=3, ncols=1)
 
 df['avstånd till framförvarande fordon (sek)'].plot(ax=axes[0])
+axes[0].set_title('Distance to Leading Vehicle')
+axes[0].set_ylabel('Distance (sec)')
+
 df['hastighet'].plot(ax=axes[1])
+axes[1].set_title('Speed')
+axes[1].set_ylabel('Speed (units)')
+
 df['acc'].plot(ax=axes[2])
+axes[2].set_title('Acceleration')
+axes[2].set_xlabel('Time')
+axes[2].set_ylabel('Acceleration (units)')
+
+plt.tight_layout()  # Adjusts the spacing between subplots
 plt.show()
